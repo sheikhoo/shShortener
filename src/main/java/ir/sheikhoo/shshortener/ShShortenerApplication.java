7defb4e91cdce6d1e3fe75b36c19b3222b13aa0e
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2022-12-09 11:24:13 +0330

    first Commit

---
+++ src/main/java/ir/sheikhoo/shshortener/ShShortenerApplication.java
@@ -0,0 +1,13 @@
+package ir.sheikhoo.shshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShShortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/sheikhoo/shshortener/ShShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sheikhoo.shshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
